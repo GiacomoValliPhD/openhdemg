08b82d81e331698205db1d7bfe1668acbfdbcc4f
Giacomo Valli_PhD <81100252+GiacomoValliPhD@users.noreply.github.com>
2023-09-05 23:00:22 +0200

    Fix dependency issues

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ import openhdemg as emg
 from pathlib import Path
 
 INSTALL_REQUIRES = [
-    "customtkinter==5.1.3",
+    "customtkinter==5.2.0",
     "matplotlib==3.7.1",
-    "numpy==1.24.3",
+    "numpy==1.25.0",
     "openpyxl==3.1.2",
-    "pandas==2.0.2",
+    "pandas==2.0.3",
     "pandastable==0.13.1",
     "pyperclip==1.8.2",
-    "scipy==1.10.1",
+    "scipy==1.11.1",
     "seaborn==0.12.2",
     "joblib==1.3.1",
 ]
